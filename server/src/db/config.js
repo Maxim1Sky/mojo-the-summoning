45d92ec39a2f980aad448a39f384d837c9f11067
Maxim Toma <maxim.toma@sky.uk>
2023-11-03 16:14:34 +0000

    Installed all npm dependencies and created the database

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,16 @@
 // add your database connection here
+const path = require("path");
+const { Sequelize, DataTypes, Model } = require("sequelize");
+
+const db = new Sequelize({
+  dialect: "sqlite",
+  storage: path.join(__dirname, "db.sqlite"),
+  logging: false,
+  define: {
+    timestamps: false,
+  },
+});
+
+db.authenticate();
+
+module.exports = { db, DataTypes, Model };
